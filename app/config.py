705b28152a5bf4609b79dc55785b9c78948691ba
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-02-12 09:58:18 +0100

    make api key optional

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     PROJECT_TITLE: str = "FastAPI Weather API Service"
     PROJECT_VERSION: str = "1.0.0"
 
-    OPEN_WEATHER_API_KEY: str
+    OPEN_WEATHER_API_KEY: str | None = None
 
     class Config:
         env_file = ".env"
